b5a71a2dc69d0edd42b6a4bcc9df53b8c7400311
Larry Athey <panhandleponics@larryathey.com>
2025-07-07 19:36:55 -0600

    Create Locomotive_Breath.ino

---
+++ Locomotive_Breath/Locomotive_Breath.ino
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------------------------------
+// LCC - Locomotive Breath | (CopyLeft) 2025-Present | Larry Athey (https://panhandleponics.com)
+//
+// Designed for the Waveshare ESP32-S3 Mini (ESP32-S3FH4R2) development board
+//
+// This is a custom LoRa-CMD+CTRL (LCC) device that bridges the LCC system with the model railroad
+// hobby. No, this is not another version of DCC or WCC, the LCC name is just a happy coincidence.
+// However, LCC can actualy do everything that DCC/WCC can do, plus a hell of a lot more.
+//
+// This ESP32 project is a collection of components that can be mounted in the top of an HO scale
+// (or larger) locomotive body to accomplish everything that you can do with a DCC/WCC locomotive
+// for not a whole lot of money. These components are as follows:
+//
+//   ESP32-S3 Mini       - $10.00
+//   Reyax RYLR998 Modem - $12.00
+//   TB6612FNG H-Bridge  - $3.50
+//   TSOP34838 IR Rcvr   - $1.00
+//   MAX98357 Audio Mod  - $2.00
+//   10x15mm Speaker     - $0.80
+//   3.3v 3A Regulator   - $0.70
+//
+// Roughly $30 in parts to convert any brand of model train locomotive to have all of the features
+// found in a full blown DCC enabled locomotive with sound effects. You want color changing LEDs?
+// That can be added too for a couple dollars. You name it, this system can be expanded to do it.
+//------------------------------------------------------------------------------------------------
+void setup() {
+
+}
+//------------------------------------------------------------------------------------------------
+void loop() {
+
+}
+//------------------------------------------------------------------------------------------------
