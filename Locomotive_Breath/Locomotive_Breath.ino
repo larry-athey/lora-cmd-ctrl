fcf8b452a73691f4fe07bb7207a7fd85da424389
Larry Athey <panhandleponics@larryathey.com>
2025-07-08 23:58:42 -0600

    Update Locomotive_Breath.ino

--- Locomotive_Breath/Locomotive_Breath.ino
+++ Locomotive_Breath/Locomotive_Breath.ino
@@ -107,9 +107,9 @@ void setup() {
   pinMode(LIMIT_2,INPUT_PULLUP);
   pinMode(OUT_1,OUTPUT); digitalWrite(OUT_1,LOW);
   pinMode(OUT_2,OUTPUT); digitalWrite(OUT_2,LOW);
-  pinMode(MOT_F,OUTPUT); digitalWrite(MOT_F,LOW);
-  pinMode(MOT_R,OUTPUT); digitalWrite(MOT_R,LOW);
-  pinMode(MOT_PWM,OUTPUT); digitalWrite(MOT_PWM,LOW);
+  pinMode(MOT_F,OUTPUT); digitalWrite(MOT_F,LOW); // AIN1 (Standby is pulled high to enable the driver)
+  pinMode(MOT_R,OUTPUT); digitalWrite(MOT_R,LOW); // AIN2
+  pinMode(MOT_PWM,OUTPUT); digitalWrite(MOT_PWM,LOW); // PWMA
 
   // Initialize the motor speed/direction controller
   ledcSetup(MOT_PWM,20000,8); // 20 KHz, 8 bit resolution
@@ -164,10 +164,10 @@ void setup() {
 void loop() {
 
   // Play .wav file from SPIFFS one time if one isn't already playing
-  //if (! Sound.isRunning()) Sound.connecttoFS(SPIFFS,"/test.wav"); 
+  if (! Sound.isRunning()) Sound.connecttoFS(SPIFFS,"/test.wav"); 
 
   // Keep the loaded .wav file playing repeatedly
-  //Sound.loop();
+  Sound.loop();
 
 }
 //------------------------------------------------------------------------------------------------
