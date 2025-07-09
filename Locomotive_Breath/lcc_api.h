b0cc487f5a5c825e16ecf05ac944f189f1489cff
Larry Athey <panhandleponics@larryathey.com>
2025-07-08 23:29:31 -0600

    Create lcc_api.h

---
+++ Locomotive_Breath/lcc_api.h
@@ -0,0 +1,5 @@
+//------------------------------------------------------------------------------------------------
+// Climate Czar Combo Hub v2 | (CopyLeft) 2022-Present | Larry Athey (https://panhandleponics.com)
+//
+// Inline functions used for modular unit organization
+//------------------------------------------------------------------------------------------------
